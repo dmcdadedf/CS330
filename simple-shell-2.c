0d98dd93ba96295ea299dd4da96c3658919f0e40
dmcdadedf <dmcdadedf@gmail.com>
2023-02-19 01:31:16 -0500

    Create simple-shell-2.c

---
+++ simple-shell-2.c
@@ -0,0 +1,465 @@
+/**
+ * Simple shell interface program.
+ *
+ * Operating System Concepts - Ninth Edition
+ * Copyright John Wiley & Sons - 2013
+ */
+
+/* For the first half of the shell project, the part that was done in simple-shell.outline1, we compiled with: 
+ *    gcc simple-shell.c -o simple-shell -s
+ * But for the second half of the project, the part described here in simple-shell.outline2, compile with: 
+ *    gcc simple-shell.c CircularArray.c -o simple-shell -s
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+//* Here you need to include the header file for the circular array. Put this on the next line by iteslf:  #include "CircularArray.h"
+//* By doing so, a global variable with be added named HistoryData that has room for 10 strings of data.
+
+#include "CircularArray.h"         //Benjamin Hill
+
+#define MAX_LINE    80 /* 80 chars per line, per command */
+
+#define READ_END    0
+#define WRITE_END   1
+
+int const HISTORY_ITEMS = 10;   // Global constant for the max number of history items.
+
+// function prototypes
+int parse_command(char command[], char *args[]);
+int check_redirect(char *args[], char op[], int len);
+int check_pipe(char *args[], int len);
+void handle_redirect_output(char *args[], int pos);
+void handle_redirect_input(char *args[], int pos);
+void handle_pipe(char *args[], int pos, int len);
+
+int main(void)
+{
+    char *args[MAX_LINE/2 + 1]; /* command line (of 80) has max of 40 arguments */
+    int should_run = 1;         /* This is a true in C. */
+    int command_length;
+    pid_t child;
+
+    int redirect_position;      // position of '>' character: what index is the > character at in the array    
+    int pipe_position;          // position of '|' character: what index is the | character at in the array
+    //***** NEW: declare an integer variable n. Its value will indicate that the desired history item is n items back.
+
+    char command[MAX_LINE]; // the command that was entered
+    char history[MAX_LINE]; //***** NEW: delete this line since we do not want just one string of history and replace this
+                            //***** line with a call to the HistoryInit function defined in CircularArray.c.
+    char str[MAX_LINE];   //**** NEW: use this variable below to hold the command obtained from history.
+
+    //*** Old directions from simple-shell.outline1, the one that handled only 1 item of history:
+    //*** Use a history holding a single string on your first attempt. That's what the command
+    //*** string variable above is for. After you get this simple shell to work with that
+    //*** (by filling in the needed code below), come back to this. Save a backup of that
+    //*** working simple-shell.c file (for example by using:  cp simple-shell.c simple-shell.first)
+    //*** and modify the original simple-shell.c to use the circular array to hold up to 10 items
+    //*** of history. That would entail commenting off the declaration of the history string just
+    //*** above and replacing it by the use of the function HistoryInit.  Make sure that all uses
+    //*** of the old history string are commented off below and that you use HistoryInsert and
+    //*** HistoryLookup instead.  You can look at CircularArray.c to see what these functions do
+    //*** and what parameters they take. The HistoryInit function is only used once, right here
+    //*** before we hit the while loop for the shell.  If you get stuck, contact Br. David.
+
+    int count = 0;
+        
+    // This is the main loop for the shell interface. This allows the shell to repeatedly show the prompt, get the user's command,
+    // and execute that command. Then loop around and do the same steps over and over.
+    while (should_run){   
+        //*** print your prompt here. It could be as simple as >
+        printf("csh> ");        //Benjamin Hill
+        fflush(stdout);
+
+        //*** DO THIS: Use fgets to read into command what the user typed in. Look up fgets if you don't know it.
+        fgets(command, MAX_LINE, stdin);        //Benjamin Hill
+
+        /**
+         * Special case - make sure "!!" isn't the first command that is entered. The !! is a command to bring back and run
+         * the last command. The count == 0 means that no commands have yet been run.
+         */
+        if ( (strncmp(command,"!!",2) == 0) && (count == 0) )  //*** This is a string compare function. Look up the details.
+        {
+            fprintf(stderr, "No command history found\n");
+            continue;   
+        }
+        else
+        {
+            count = 1;   //*** This is the shell's first command.
+        }
+    
+        /**
+          * Another special case - make sure "!-n", where n is a positive integer,
+          * isn't the first command that is entered.
+          */
+        if ( (strncmp(*** NEW: fill this in ***) == 0) && (count == 0) )
+        {
+            fprintf(stderr, "No command history found\n");
+            continue;
+        }
+        else
+        {
+            count = 1;
+        }
+
+        //*** NEW: This section deals with the history, but does not execute the user's command.
+        if ((strncmp(command,"!!", 2) != 0) && (strncmp(command,"!-",2) != 0))
+        {
+            /**
+             * We have entered a command so copy the command to history.
+             */
+            //**** NEW: Get rid of this strcpy and use the HistoryInsert function to put the command into the circular array.
+            strcpy(history,command);
+        }
+        else if (strncmp(command,"!!",2) == 0)
+        {
+            // we use the most recent command
+            //**** NEW: Use the HistoryLookup function to find the most recent command, print it to the screen, copy it to the command
+            //**** variable so we can execute it below, and also use HistoryInsert to insert it into history, thus giving the
+            //**** correct history that we ran this command twice in a row.
+
+
+
+        }
+        else if (strncmp(command, "!-", 2) == 0)
+        {
+            //**** NEW: This code has been done for you. It removes the first two characters, the !- from the command string.
+            char temp[MAX_LINE];
+            int len, k, m, num;
+            len = strlen(command);
+            for (k = 2, m = 0; k < len; k++, m++)
+               temp[m] = command[k];
+            temp[len] = '\0';
+            //**** NEW: temp now contains the command string without the !-. This should be an integer since the user used !-n.
+            // printf("DEBUG: number after the !- is %s", temp);
+            //**** NEW: Use the atoi function to convert temp to an integer, placing it into variable num.
+            //**** NEW: Use HistoryLookup with num to look up the item in history num items back.
+            //**** Then print that history item on the screen, copy it into the command variable so that we can execute it below,
+            //**** and use HistoryInsert to get this item into the history circular array, much like was done above.
+            //**** Thus !-1 would execute the last command, the same as !!, while !-2 would run the command 2 back in history,
+            //**** !-3 would run the command 3 back in history, etc.
+
+
+        }
+
+        if (strncmp(command, "history", 7) == 0)   //**** NEW: Display all the items in history.
+        {                                          //**** This helps in debugging, but is also helpful to the user.
+            //**** NEW: HistoryCount is a global variable holding the number of items currently in history.
+            //**** Use that HistoryCount variable to write a loop that has an integer k loop through 1, 2, ..., HistoryCount.
+            //**** For each value of k, use the HistoryLookup function to look up the kth item in history.
+            //**** Print the value of k and the kth history item on one line. Do the same for the next value of k, etc. 
+               }
+            continue;   //*** Go to the top of the main loop as we do not want to execute anything further here.
+        }
+       
+        // Break command up into tokens which are placed in args. command_length will hold the number of tokens.
+        command_length = parse_command(command, args);  
+
+        if (command_length == 0)
+        {
+            continue;   //*** Nothing to execute. Skip the rest of the loop body and go back to the top of the loop.
+        }
+
+        if (strcmp(args[0],"exit") == 0) 
+        {
+            break;   //*** Break out of the loop and thus the shell ends.
+        }
+
+        if (strcmp(args[0],"!!") == 0)
+        {
+            //strcpy(command, history);  **** NEW: Replace this strcpy with a call to HistoryLookup to find the last command
+                                       //**** and put it into the command string. There are no changes to make beyond this point
+                                       //**** if you successfully completed the first version of the simple shell, the one with
+                                       //**** only 1 item of history, though you would have to copy into here your code for the
+                                       //**** 2 places below that have only an outline. If you did not finish that code, now is
+                                       //**** the time to figure it out and put it in below.
+
+
+        //*** This section handles output redirection, as in ls -l > filename
+        if ( (redirect_position = check_redirect(args, ">", command_length)) > 0)
+        {
+            handle_redirect_output(args, redirect_position);
+    
+            continue;
+        }        
+        
+        //*** DO THIS:  Since > is for redirecting output and < for redirecting input in bash,
+        //*** handle < in a similar way to how > was handled above.  Put that code right here:
+ 
+        if ((redirect_position = check_redirect(args, "<", command_length)) > 0)            //David McDade
+        {
+            handle_redirect_input(args, redirect_position);
+
+            continue;
+        }
+
+
+        //*** The next section handles a pipe, the | symbol. For example, the command typed in and
+        //*** entered might be something like:  ls | wc
+        //*** That one pipes a listing of files into the wordcount program, telling you how many
+        //*** words (and characters and lines) are in the output. This section is finished.        
+        if ( (pipe_position = check_pipe(args, command_length)) > 0)
+        {
+            handle_pipe(args, pipe_position, command_length);
+    
+            continue;
+        }
+
+        //*** Fork off a child process to handle the execution of the current command.
+        //*** Meanwhile the parent should wait for the child to finish.
+        child = fork();
+
+        if (child == 0)
+        {
+            // child process
+            args[command_length] = NULL;
+            execvp(args[0], args);          //*** Here is where the new command is executed.
+
+            fprintf(stderr, "Command not found\n");  //*** If the exec worked, this message will not be seen.
+            break;
+        }
+        else if (child > 0)
+        {
+            //*** DO THIS: This is the parent process. Have it wait here for the child to complete.
+            wait(NULL);         //Benjamin Hill
+
+        }
+        else 
+        {
+            fprintf(stderr, "Error forking child\n");
+            return -1;
+        }
+    }
+    
+    return 0;
+}
+
+//******* Everything after this point is complete. You may however, have to call some of these
+//******* functions at some spots in your code above. Some such calls are already present.
+
+/**
+ * Parses the command and places each token into the args array.
+ * The count of the number of args is returned in the function name.
+ */
+int parse_command(char command[], char *args[])
+{
+    char *spart = strtok(command, " \n");   //*** strtok function breaks a token off from the command string...
+    int length = 0;                         //*** ...here with space or newline as separators.
+
+    while (spart != NULL)
+    {
+        args[length] = spart;
+        length++;
+        spart = strtok(NULL, " \n");  // The NULL tells strtok to continue where it left off, that is, it breaks off another token.
+    }
+
+    return length;
+}
+
+/**
+ * Checks if there is file input redirection (op, short for operator, is the < or > symbol) and return its location.
+ */
+int check_redirect(char *args[], char op[], int len)
+{
+    int rv = 0;
+    int i = 0;
+
+    for (i = 0; i < len; i++) 
+    {
+        if ( strcmp(args[i],op) == 0)
+        {
+            rv = i;
+            break;
+        }
+    }
+
+    return rv;
+}
+
+/**
+ * Checks if there is a pipe symbol and returns its location.
+ */
+int check_pipe(char *args[], int len)
+{
+    int rv = 0;
+    int i = 0;
+
+    for (i = 0; i < len; i++) 
+    {
+        if ( strcmp(args[i],"|") == 0)
+        {
+            rv = i;
+            break;
+        }
+    }
+
+    return rv;
+}
+
+/**
+ * Handles the situation of redirected output.
+ *
+ * pos is the position in the args array where the '>' occurs.
+ */
+void handle_redirect_output(char *args[], int pos)
+{
+    pid_t child;
+    int fd;
+
+    child = fork();
+
+    if (child == 0)
+    {
+        // child process
+        args[pos] = NULL;
+    
+        fd = open(args[pos+1], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+
+        if (fd == -1) {
+            fprintf(stderr,"OPEN ERROR\n");
+            exit(1);
+        }
+    
+        // the file descriptor is duped to standard output
+        if (dup2(fd, STDOUT_FILENO) == -1) {
+            fprintf(stderr,"DUP ERROR\n");
+            exit(1);
+        }
+
+        // close the original file descriptor
+        if (close(fd) == -1) {
+            fprintf(stderr,"CLOSE ERROR\n");
+            exit(1);
+        }
+
+        execvp(args[0], args);   //*** Run the command.
+
+        fprintf(stderr, "Command not found\n");   //*** This only prints if exec fails.
+    }
+    else if (child > 0)
+    {
+        // This is really the parent process, so wait for the child to finish.
+        wait(NULL);
+    }
+    else 
+    {
+        fprintf(stderr, "Error forking child\n");
+    }
+}
+
+/**
+ * Handles the situation of redirected input.
+ *
+ * pos is the position in the args array where the '<' occurs.
+ */
+void handle_redirect_input(char *args[], int pos)
+{
+    pid_t child;
+    int fd;
+
+    child = fork();
+
+    if (child == 0)
+    {
+        // child process
+        args[pos] = NULL;
+    
+        fd = open(args[pos+1], O_RDONLY);
+
+        if (fd == -1) {
+            fprintf(stderr,"OPEN ERROR\n");
+            exit(1);
+        }
+    
+        // the file descriptor has been duped to standard input
+        if (dup2(fd, STDIN_FILENO) == -1) {
+            fprintf(stderr,"DUP ERROR\n");
+            exit(1);
+        }
+
+        // close the original file descriptor
+        if (close(fd) == -1) {
+            fprintf(stderr,"CLOSE ERROR\n");
+            exit(1);
+        }
+
+        execvp(args[0], args);   //*** Run the command
+
+        fprintf(stderr, "Command not found\n");   //*** This only prints if exec fails.
+    }
+    else if (child > 0)
+    {
+        // This is really the parent process, so wait for the child to finish.
+        wait(NULL);
+    }
+    else 
+    {
+        fprintf(stderr, "Error forking child\n");
+    }
+}
+
+
+/**
+ * Handles the situation of a pipe.
+ *
+ * pos is the position in the args array where the '|' occurs.
+ */
+void handle_pipe(char *args[], int pos, int len)
+{
+    pid_t child_1;
+    pid_t child_2;
+    int fd[2];
+
+    int i;
+
+    child_1 = fork();
+
+    if (child_1 == 0) {
+        // child process ... soon to be a parent!
+        if (pipe(fd) == -1) {
+            fprintf(stderr,"CLOSE ERROR\n");
+            return;
+        }
+
+        // have the child fork another child
+        child_2 = fork();
+
+        if (child_2 > 0) {
+            // parent
+            close(fd[READ_END]);
+            close(STDOUT_FILENO);
+
+            dup2(fd[WRITE_END],STDOUT_FILENO);
+
+            // now exec the first command
+            args[pos] = NULL;
+            execvp(args[0], args);
+        }
+        else if (child_2 == 0) {
+            // child 2
+            close(fd[WRITE_END]);
+            //close(STDIN_FILENO);
+
+            dup2(fd[READ_END],STDIN_FILENO);
+
+            // now exec the second command
+            execvp(args[pos+1],&args[pos+1]);
+        }
+        else {
+            fprintf(stderr, "Error forking child\n");
+        }
+    }
+    else if (child_1 > 0) {
+        // parent process
+        wait(NULL);
+    }
+    else 
+    {
+        fprintf(stderr, "Error forking child\n");
+    }
+}
